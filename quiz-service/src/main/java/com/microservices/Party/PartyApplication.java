dd18559defdbfd305ae67b0cf0fdc009ab603f6d
Surya-2020 <suryadonald@gmail.com>
2024-05-02 22:56:35 +0530

    updated commit

--- quiz-service/src/main/java/com/microservices/Party/PartyApplication.java
+++ quiz-service/src/main/java/com/microservices/Party/PartyApplication.java
@@ -2,10 +2,12 @@ package com.microservices.Party;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class PartyApplication {
 
 	public static void main(String[] args) {
